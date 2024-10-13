8c6d13ddec1856b0ede4d309ae8e792b4916e895
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-10-13 05:32:38 +0200

    Update thunder.js

--- static/thunder.js
+++ static/thunder.js
@@ -1,30 +1,27 @@
-// Tableau des sons disponibles
+// Liste des sons d'orage
+const thunderSounds = [
+    "{{ url_for('static', filename='loud-thunder-192165.mp3') }}",
+    "{{ url_for('static', filename='thunder.mp3') }}",
+    "{{ url_for('static', filename='rain-and-thunder-sfx-12820.mp3') }}"
+];
 
-// Fonction pour jouer un son d'orage aléatoire après un délai aléatoire
+// Fonction pour jouer un son d'orage aléatoire
 function playThunderSound() {
-  const thunderSound = document.getElementById('thunder-sound');
+    const randomSound = thunderSounds[Math.floor(Math.random() * thunderSounds.length)];
+    const thunderSound = new Audio(randomSound);
 
-  // Sélectionne un son aléatoirement
-  const randomSound = thunderSounds[Math.floor(Math.random() * thunderSounds.length)];
+    // Générer un délai aléatoire entre 20 et 60 secondes (20000 à 60000 millisecondes)
+    const randomDelay = Math.random() * (60000 - 20000) + 20000;
 
-  // Change la source de l'élément audio
-  thunderSound.src = randomSound;
-
-  // Réduire le volume à 50%
-  thunderSound.volume = 0.5;
-
-  // Générer un délai aléatoire entre 20 et 60 secondes (20000 à 60000 millisecondes)
-  const randomDelay = Math.random() * (60000 - 20000) + 20000;
-
-  // Jouer le son après le délai
-  setTimeout(function() {
-    thunderSound.play();
-    // Après avoir joué le son, rejoue de manière aléatoire
-    playThunderSound();
-  }, randomDelay);
+    // Jouer le son après le délai
+    setTimeout(function() {
+        thunderSound.play();
+        // Après avoir joué le son, rejoue de manière aléatoire
+        playThunderSound();
+    }, randomDelay);
 }
 
 // Lancer la première lecture du son quand la page est complètement chargée
 window.addEventListener('load', function() {
-  playThunderSound();
+    playThunderSound();
 });
